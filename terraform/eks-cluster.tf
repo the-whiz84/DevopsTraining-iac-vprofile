660b61b271bb48d61689f46ef54c035a45b56bf8
Radu Chiriac <radu@zerodawn.tech>
2026-01-11 17:02:49 +0200

    Update TF config file for EKS module

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,22 +1,18 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 21.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  name               = "example"
+  kubernetes_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
+  endpoint_public_access         = true
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      ami_type       = "AL2023_x86_64_STANDARD"
+      name           = "node-group-1"
 
       instance_types = ["t3.small"]
 
@@ -26,7 +22,8 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
+      ami_type       = "AL2023_x86_64_STANDARD"
+      name           = "node-group-2"
 
       instance_types = ["t3.small"]
 
