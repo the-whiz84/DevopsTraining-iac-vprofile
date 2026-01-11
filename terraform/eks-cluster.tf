99e506906bfbd68748ea5db04d152fce38a824b5
Radu Chiriac <radu@zerodawn.tech>
2026-01-11 22:57:30 +0200

    Update kubernetes version

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0"
 
   name               = local.cluster_name
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.34"
 
   vpc_id                 = module.vpc.vpc_id
   subnet_ids             = module.vpc.private_subnets
